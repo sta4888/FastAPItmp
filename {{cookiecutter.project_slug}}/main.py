7b6b0c32e2895e6453b09f477061d0f5b7a0abe6
Sta4 <talonin92@mail.ru>
2025-06-04 15:25:26 +0500

    v0.0.0.1

---
+++ {{cookiecutter.project_slug}}/main.py
@@ -0,0 +1,16 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+{% if cookiecutter.app_type == "async" %}
+@app.get("/")
+async def root():
+    return {"message": "Hello from async"}
+{% else %}
+@app.get("/")
+def root():
+    return {"message": "Hello from sync"}
+{% endif %}
